93e1bbc8a6f1f211c33cd774806eb0e932b02b21
Dolores <alyciahathaways@gmail.com>
2018-08-28 00:04:04 +0800

    加入 parcel，将 button 组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        /* 按钮不要写死宽度，用 padding 撑开 */
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-background);
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+
+</style>
