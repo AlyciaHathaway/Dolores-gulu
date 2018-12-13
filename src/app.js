e33f3eca1e991640c70300e968964f5c7aacb81a
Dolores <alyciahathaways@gmail.com>
2018-12-13 12:34:04 +0800

    使用 chai 添加四个单元测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -15,3 +15,93 @@ new Vue({
 	    loading3: false
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+// 单元测试
+
+{
+	// 把对象改为构造函数
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: 'setting'
+		}
+	})
+	gButton.$mount()
+	let useElement = gButton.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-setting')
+	gButton.$el.remove()
+	gButton.$destroy()
+}
+
+{
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: 'setting',
+			loading: true
+		}
+	})
+	gButton.$mount()
+	let useElement = gButton.$el.querySelector('use')
+	let href = useElement.getAttribute('xlink:href')
+	expect(href).to.eq('#i-loading')
+	gButton.$el.remove()
+	gButton.$destroy()
+}
+
+
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: 'setting'
+		}
+	})
+	gButton.$mount(div)
+	let svg = gButton.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq('1')
+	gButton.$el.remove()
+	gButton.$destroy()
+}
+
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: 'setting',
+			iconPosition: 'right'
+		}
+	})
+	gButton.$mount(div)
+	let svg = gButton.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq('2')
+	gButton.$el.remove()
+	gButton.$destroy()
+}
+
+{
+	// mock
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: 'setting',
+			iconPosition: 'right'
+		}
+	})
+	gButton.$mount()
+	gButton.$on('click', function() {
+		console.log(1)
+	})
+	// 希望这个函数被执行
+	let button = gButton.$el
+	button.click()
+}
