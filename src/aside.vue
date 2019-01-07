6c3baa29a636eeeb85cea27f8156f7e71a90aeeb
Dolores <alyciahathaways@gmail.com>
2019-01-07 16:53:19 +0800

    布局的基本功能完成

---
+++ src/aside.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="aside">
+		<slot></slot>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'GuluAside'
+	}
+</script>
+
+<style lang="scss" scoped>
+	.aside {
+	
+	}
+</style>
---
+++ src/layout.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="layout" :class="layoutClass">
+		<slot></slot>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+		name: 'GuluLayout',
+		data() {
+			return {
+				layoutClass: {
+					hasAside: false
+				}
+			}
+		},
+		mounted() {
+			this.$children.map((vm)=> {
+				if (vm.$options.name === 'GuluAside') {
+					this.layoutClass.hasAside = true
+				}
+			})
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.layout {
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+		border: 1px solid red;
+		&.hasAside {
+			flex-direction: row;
+		}
+	}
+</style>
