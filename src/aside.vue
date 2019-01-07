0d50ca3aa4650dc786ed2201c54404859e93b1b2
Dolores <alyciahathaways@gmail.com>
2019-01-07 18:10:44 +0800

    添加简单的侧边栏伸缩动画

--- src/aside.vue
+++ src/aside.vue
@@ -1,18 +1,36 @@
 <template>
-	<div class="aside">
-		<slot></slot>
-	</div>
-
+	<transition name="slide">
+		<div class="aside" v-if="visible">
+			<slot></slot>
+			<g-button @click="visible=false">close</g-button>
+		</div>
+	</transition>
 </template>
 
 <script>
 	export default {
-		name: 'GuluAside'
+		name: 'GuluAside',
+		data() {
+			return {
+				visible: true
+			}
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	.aside {
-	
+		position: relative;
+		> .g-button {
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+	}
+	.slide-enter-active, .slide-leave-active {
+		transition: all .5s;
+	}
+	.slide-enter, .slide-leave-to {
+		margin-left: -200px;
 	}
 </style>
