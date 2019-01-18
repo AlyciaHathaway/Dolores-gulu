20c099c5654bc8a94345c71a8dc783e73dc94fc1
Dolores <alyciahathaways@gmail.com>
2019-01-18 17:42:21 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -96,7 +96,12 @@
 	$font-size: 14px;
 	$toast-min-height: 40px;
 	$toast-bg: rgba(0, 0, 0, 0.75);
+	@keyframes fade-in {
+		0% {opacity: 0;}
+		100% {opacity: 1;}
+	}
 	.toast {
+		animation: fade-in 1s;
 		position: fixed;
 		left: 50%;
 		display: flex;
