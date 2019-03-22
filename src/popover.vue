764a977f773e14db101fb5415f04c2751d2544b0
Dolores <alyciahathaways@gmail.com>
2019-03-22 14:48:20 +0800

    解决当容器有 overflow:hidden 时造成的 popover 被覆盖的问题

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class="popover" @click.stop="xxx">
-		<div class="content-wrapper" v-if="visible" @click.stop>
+		<div class="content-wrapper" ref="contentWrapper" v-if="visible">
 			<slot name="content"></slot>
 		</div>
-		<slot></slot>
+		<span ref="triggerWrapper">
+			<slot></slot>
+		</span>
+		
 	</div>
 </template>
 
@@ -20,6 +23,11 @@
 				this.visible = !this.visible
 				if (this.visible === true) {
 					this.$nextTick(() => {
+						// 父元素 overflow: hidden 时，popover会无法显示，所以把它放到 body 里
+						document.body.appendChild(this.$refs.contentWrapper)
+						let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+						this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+						this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
 						let eventHandler = ()=> {
 							this.visible = false
 							document.removeEventListener('click', eventHandler)
@@ -37,13 +45,12 @@
 		position: relative;
 		display: inline-block;
 		vertical-align: top;
-		
-		.content-wrapper {
-			position: absolute;
-			bottom: 100%;
-			left: 0;
-			border: 1px solid red;
-			box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-		}
+	}
+	
+	.content-wrapper {
+		position: absolute;
+		transform: translateY(-100%);
+		border: 1px solid red;
+		box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
 	}
 </style>
